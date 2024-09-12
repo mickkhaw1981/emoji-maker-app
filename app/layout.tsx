612de7be09aa31c03064a49f3b883c7607455d23
Mike Khaw <mikekhawnow@gmail.com>
2024-09-12 08:38:35 -0700

    added authentication via clerk.dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs';
+import dynamic from 'next/dynamic';
+
+// Dynamically import the Header component with SSR disabled
+const Header = dynamic(() => import('@/components/ui/headers'), { ssr: false });
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,12 +29,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Header />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
